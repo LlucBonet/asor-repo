987b8da615d844fb46769a854b7d3d902da29415
Kali <mbonet03@ucm.es>
2021-10-25 05:24:41 -0400

    Comentar ejercicios 1 y 2

--- practica2.1/ejercicio1.cc
+++ practica2.1/ejercicio1.cc
@@ -2,8 +2,10 @@
 #include <unistd.h>
 #include <stdio.h>
 
+//Gestionar correctamente los errores generados por la llamada a 
+//setuid(2)
 int main() {
-	int exito = setuid(0);
+	int exito = setuid(0); //1: exito, -1: error
 	if(exito == -1) {
 		perror("ERROR setuid(2)");
 	}
--- practica2.1/ejercicio2.cc
+++ practica2.1/ejercicio2.cc
@@ -4,10 +4,13 @@
 #include <errno.h>
 #include <string.h>
 
+//Imprimir el código de error generado por setuid(2) tanto en su 
+//verión numérica como la cadena asociada
 int main() {
 	int exito = setuid(0);
 	if(exito == -1) {
-		printf("setuid(2) ERROR %d: %s\n", errno, strerror(errno));
+		printf("setuid(2) ERROR %d: %s\n", 
+		errno, strerror(errno));
 	}
 	return 1;
 }
