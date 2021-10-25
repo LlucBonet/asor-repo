0fc2fa989a2f7d8e3adabc34acc2bc6d48f9899a
Kali <mbonet03@ucm.es>
2021-10-25 05:53:59 -0400

    Ejercicio 4

---
+++ practica2.1/ejercicio4.sh
@@ -0,0 +1,26 @@
+echo -e "uname -a --> Print all information. Order: kernel name, network node hostname, kernel version, machin hardware name, processor type, hardware platform, operating system:"
+uname -a
+
+echo -e "\nuname -s --> Print kernel-name:"
+uname -s
+
+echo -e "\nuname -n --> Print network node hostname:"
+uname -n
+
+echo -e "\nuname -r --> Print the kernel release:"
+uname -r
+
+echo -e "\nuname -v --> Print the kernel verion:"
+uname -v
+
+echo -e "\nuname -m --> Print the machine hardware name:"
+uname -m
+
+echo -e "\nuname -p --> Print the processor type:"
+uname -p
+
+echo -e "\nuname -i --> Print the hardware platform:"
+uname -i
+
+echo -e "\nuname -o --> Print the operating system:"
+uname -o
